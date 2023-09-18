6af2b679acc036ac8a6ae4e18c0e2b3c7c58d7f6
henry gad <henrygad.orji@gamil.com>
2023-09-18 11:21:18 +0100

    version 2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.querySelector('.root').addEventListener('click', ()=>{
     alert('hey there watch out');
-    console.log('version 1');
+    console.log('version 2');
+    console.log('this massage os for version 2 of my code')
 })
