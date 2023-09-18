6af4bdcaee8a4e7c9e76ba11a46999b65e8cc027
henry gad <henrygad.orji@gamil.com>
2023-09-18 13:49:46 +0100

    version 4

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('.root').addEventListener('click', ()=>{
     alert('hey there watch out');
-    console.log('version 3');
-    console.log('another massage for version 3 of my code')
+    console.log('version 4');
+    console.log('version 4 comes with a contact form page')
 })
