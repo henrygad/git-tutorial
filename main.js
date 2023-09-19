63ab8ced96639100418a9146f18557778a017c30
henry gad <henrygad.orji@gamil.com>
2023-09-19 17:42:26 +0100

    version 6

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('.root').addEventListener('click', ()=>{
     alert('hey there watch out');
-    console.log('version 5');
-    console.log('version 5 comes with a contact form page, and probaly a little changes')
+    console.log('version 6');
+    console.log('version 6 comes with a portfolio page')
 })
