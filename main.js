5ac0a4319bc7dc2675dbf3d6676d4d21762b08cc
henry gad <henrygad.orji@gamil.com>
2023-09-19 17:38:20 +0100

    version 5

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('.root').addEventListener('click', ()=>{
     alert('hey there watch out');
-    console.log('version 4');
-    console.log('version 4 comes with a contact form page')
+    console.log('version 5');
+    console.log('version 5 comes with a contact form page, and probaly a little changes')
 })
